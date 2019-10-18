3f95ea376b0920c60c2e976d76c054a4de23b8b2
Tutorial-bot-guider <50870164+Tutorial-bot-guider@users.noreply.github.com>
2019-10-18 17:59:41 +0530

    Update botLogin.js

--- botLogin.js
+++ botLogin.js
@@ -1,5 +1,5 @@
 module.exports = {
 	token: "NjM0MzQ4NzAwODQ3NzAyMDM4.Xamunw.qiXnLpd0KCVFAOCgjTlIZFK0mHQ",
-	youtubeAPI: "YOUTUBE_API_KEY",
+	youtubeAPI: "AIzaSyAt_7oFNtN3hDu5yOhEIccU_Y4x4lffifc",
 	owner_id: "Rakshitrana"
 }
